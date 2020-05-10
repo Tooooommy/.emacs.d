8d58ff2fcea3e3a89a2c26f7f016b8a0b8b15b15
tommy <tommy_mgt@163.com>
2020-05-11 01:34:07 +0800

    add lua

--- lisp/init-lua.el
+++ lisp/init-lua.el
@@ -2,7 +2,18 @@
 (use-package lua-mode
   :delight "Λ "
   :mode "\\.lua\\'"
-  :interpreter ("lua" . lua-mode))
+  :interpreter ("lua" . lua-mode)
+  :config
+  ;; @seee https://github.com/ptrv/company-lua
+  (use-package company-lua
+    :ensure t
+    :config
+    (defun my-lua-mode-company-init ()
+      (setq-local company-backends '((company-lua
+                                      company-etags
+                                      company-dabbrev-code
+                                      company-yasnippet))))
+    (add-hook 'lua-mode-hook #'my-lua-mode-company-init)))
 
 (provide 'init-lua)
 ;;; init-lua.el
