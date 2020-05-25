1ae42bbef8949f7779bda17493883be5228b255e
tommy <tommy_mgt@163.com>
2020-05-25 23:56:42 +0800

    cpp

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -1,41 +1,14 @@
 ;; C/C++ Mode
 (use-package cc-mode
   :ensure nil
-  :defer 3
   :bind (:map c-mode-base-map
          ("C-c c" . compile))
-  :init
-  (setq-default c-default-style "linux")
-  (setq-default c-basic-offset 4)
+  :hook (c-mode-common . (lambda () (c-set-style "stroustrup")))
+  :init (setq-default c-basic-offset 4)
   :config
-  (use-package company-ctags
-    :ensure t
-    :defer 3
-    :after company
-    :config
-    (setq company-ctags-extra-tags-files '("$HOME/TAGS" "/usr/include/TAGS"))
-    (company-ctags-auto-setup)
-    )
-
   (use-package modern-cpp-font-lock
-    :ensure t
-    :defer 3
     :diminish
-    :hook (after-init . modern-c++-font-lock-global-mode ))
-
-  (use-package google-c-style
-    :hook (c-mode-common . (lambda ()
-                             (google-set-c-style)
-                             (google-make-newline-indent))))
-
-  (use-package cmake-mode
-    :defer 3
-    :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'"))
-
-  (use-package cmake-font-lock
-    :defer 3
-    :after (cmake-mode)
-    :hook (cmake-mode . cmake-font-lock-activate)))
+    :init (modern-c++-font-lock-global-mode t)))
 
 (provide 'init-cpp)
 
